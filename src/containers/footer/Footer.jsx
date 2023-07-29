c485a10b1243b6a241c937c6560ead13a7bc7350
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-07-29 15:36:32 +0530

    adding components

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import gpt4Logo from '../../assets/logo.svg';
+import './footer.css';
 
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <div className="gpt4__footer section__padding">
+    <div className="gpt4__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
 
-export default Footer
+    <div className="gpt4__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt4__footer-links">
+      <div className="gpt4__footer-links_logo">
+        <img src={gpt4Logo} alt="gpt4_logo" />
+        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt4__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt4__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt4__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+
+    <div className="gpt4__footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
